232f5ea89369f4271f7cdff026a76b0946d7d430
Magnus Hansson <hanssmag+git@gmail.com>
2021-09-05 20:55:59 +0200

    third

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const app = express()
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+//Qs3GoYdhJ47r6o9Z garage
+//cluster0.rp737.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+const uri =
+  
+const Document = require('./models/Document')
+const mongoose = require('mongoose')
+
+mongoose.connect(uri)
+app.get('/', (req, res) => {
+  const code = `
+  welcome to copy paste artist!
+        
+  use the commands in the top right
+  to create a new file to share
+        `
+
+  res.render('code-display', {
+    code,
+    lineNumbers: code.split('\n').length,
+    language: 'plaintext'
+  })
+})
+
+app.get('/new', (req, res) => {
+  res.render('new')
+})
+
+app.get('/:id', async (req, res) => {
+  const id = req.params.id
+  try {
+    const document = await Document.findById(id)
+
+    res.render('code-display', { code: document.value, id })
+  } catch (e) {
+    res.redirect('/')
+  }
+})
+
+app.get('/:id/duplicate', async (req, res) => {
+  const id = req.params.id
+  try {
+    const document = await Document.findById(id)
+    res.render('new', { value: document.value })
+  } catch (e) {
+    res.redirect(`/${id}`)
+  }
+})
+
+app.post('/save', async (req, res) => {
+  const value = req.body.value
+  try {
+    const document = await Document.create({ value })
+    res.redirect(`/${document.id}`)
+  } catch (e) {
+    res.render('new', { value })
+  }
+})
+app.listen(3000)
